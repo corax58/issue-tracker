03f19e6b0024b0c96149b28e237c3defbe73d057
Abubeker Abduljelil <114282920+corax58@users.noreply.github.com>
2024-08-21 20:41:33 +0300

    created the issue form

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,17 @@
 "use client";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
